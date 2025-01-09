0fcdad62612adda69f0ca7402148ef4fc5a3eb09
vedant-dev <vedantdbhamare@gmail.com>
2025-01-09 12:55:20 +0530

    updated the header component

--- client/whizz-admin/src/components/Header.tsx
+++ client/whizz-admin/src/components/Header.tsx
@@ -6,11 +6,6 @@ import { HelpCircle } from "lucide-react";
 export default function Header() {
   return (
     <header className="p-2 bg-[#fff] border-b border-[#e5e7eb] flex justify-end items-center gap-4 fixed z-40 top-0 left-0 right-0">
-      {/* ShadCN Switch Component */}
-      <div className="flex items-center gap-2">
-        <Switch className="data-[state=checked]:bg-[#3CAE06]" />
-      </div>
-
       {/* Help Icon */}
       <button className="p-2 rounded-md hover:bg-gray-100">
         <HelpCircle size={24} className="text-gray-600" />
