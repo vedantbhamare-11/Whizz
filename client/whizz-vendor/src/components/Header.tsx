40c925a8e427c8df5fefa4f5f8cea95e6e74321e
vedant-dev <vedantdbhamare@gmail.com>
2025-01-27 15:50:03 +0530

    updated the header switch

--- client/whizz-vendor/src/components/Header.tsx
+++ client/whizz-vendor/src/components/Header.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { Switch } from "@/components/ui/switch";
+import { useState } from "react";
 
 export default function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleToggle = () => {
+    setIsActive((prev) => !prev);
+  };
+
   return (
     <header className="p-2 h-16 bg-[#fff] border-b border-[#e5e7eb] flex justify-end items-center gap-4 fixed z-40 top-0 left-0 right-0">
       {/* ShadCN Switch Component */}
       <div className="flex items-center gap-2">
-        <Switch className="data-[state=checked]:bg-[#3CAE06]" />
+        <span>Restaurant{" "}{isActive ? "Active" : "Inactive"}</span>
+        <Switch
+          checked={isActive}
+          onCheckedChange={handleToggle}
+          className="data-[state=checked]:bg-[#3CAE06] mr-12"
+        />
       </div>
-
-     
     </header>
   );
 }
